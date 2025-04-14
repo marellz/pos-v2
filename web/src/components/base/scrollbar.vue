bc61908542fbd96410287717af198b38138b2332
Dave Njoroge <davenjoroge10@gmail.com>
2025-04-14 18:30:58 +0300

    web: add scrollbar component

---
+++ web/src/components/base/scrollbar.vue
@@ -0,0 +1,9 @@
+<template>
+<PerfectScrollbar>
+    <slot />
+</PerfectScrollbar>
+</template>
+<script lang="ts" setup>
+import { PerfectScrollbar } from 'vue3-perfect-scrollbar';
+import 'vue3-perfect-scrollbar/style.css'
+</script>
